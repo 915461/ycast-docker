a350d1acb3c8e206c1d088fe92081d992c207a59
Jože M. <42513356+915461@users.noreply.github.com>
2025-12-28 21:06:41 +0100

    Added note on required privileges.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-#Bootstrap File for ycast docker container
-#Variables
-#YC_VERSION version of ycast software
-#YC_STATIONS path an name of the indiviudual stations.yml e.g. /ycast/stations/stations.yml
-#YC_DEBUG turn ON or OFF debug output of ycast server else only start /bin/sh
-#YC_PORT port ycast server listens to, e.g. 80
-#YC_ADDRS known addresses of *.vtuner.com. For Denon: YC-ADDRS="8.38.76.252 154.27.73.59 23.238.108.243)
+# Bootstrap File for ycast docker container
+# Variables
+# YC_VERSION version of ycast software
+# YC_STATIONS path an name of the indiviudual stations.yml e.g. /ycast/stations/stations.yml
+# YC_DEBUG turn ON or OFF debug output of ycast server else only start /bin/sh
+# YC_PORT port ycast server listens to, e.g. 80
+# YC_ADDRS known addresses of *.vtuner.com. For Denon: YC-ADDRS="8.38.76.252 154.27.73.59 23.238.108.243)
+# Docker image must have elevated privileges: NET_ADMIN
+
 
 for rg in $YC_ADDRS; do
     ip addr add $rg dev eth0
