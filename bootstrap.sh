cb04b1e63bd3abe8754dfb3e15998cc40f5daa76
Jože M. <42513356+915461@users.noreply.github.com>
2025-12-28 21:05:08 +0100

    Additional eth0 addresses set in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-#Bootstrap File für ycast docker container
+#Bootstrap File for ycast docker container
 #Variables
 #YC_VERSION version of ycast software
 #YC_STATIONS path an name of the indiviudual stations.yml e.g. /ycast/stations/stations.yml
 #YC_DEBUG turn ON or OFF debug output of ycast server else only start /bin/sh
 #YC_PORT port ycast server listens to, e.g. 80
+#YC_ADDRS known addresses of *.vtuner.com. For Denon: YC-ADDRS="8.38.76.252 154.27.73.59 23.238.108.243)
+
+for rg in $YC_ADDRS; do
+    ip addr add $rg dev eth0
+done
 
 if [ "$YC_DEBUG" = "OFF" ]; then
 	/usr/bin/python3 -m ycast -c $YC_STATIONS -p $YC_PORT
@@ -17,8 +22,3 @@ else
 	/bin/sh
 
 fi
-
-
-
-
-
